6795b4a341db7d27fa78f8ae6a77cc63db079b57
Craig Harrison <craigharrison112@gmail.com>
2018-03-27 02:55:33 -0400

    Simplified delete button

--- client/src/meditate/index.jsx
+++ client/src/meditate/index.jsx
@@ -4,14 +4,7 @@ import { render } from 'react-dom';
 const Meditate = () => (
   <section className="section">
     <div className="container">
-      <div className="level">
-        <div className="level-left">
-          <div className="level-item">
-            <a href="/profile" className="delete is-large" />
-          </div>
-        </div>
-        <div className="level-right" />
-      </div>
+      <a href="/profile" className="delete is-large" />
     </div>
   </section>
 );
